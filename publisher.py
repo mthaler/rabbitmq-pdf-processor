c49e1b34773bcf2911fcb3ef67cc9c8f991f7fdf
Michael Thaler <michael.thaler@dlr.de>
2021-05-17 10:00:01 +0200

    Added more screenshots.

---
+++ publisher.py
@@ -0,0 +1,18 @@
+import logging
+import os
+
+logging.basicConfig()
+
+# Parse CLODUAMQP_URL (fallback to localhost)
+url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost/%2f')
+params = pika.URLParameters(url)
+params.socket_timeout = 5
+
+connection = pika.BlockingConnection(params) # Connect to CloudAMQP
+channel = connection.channel() # start a channel
+channel.queue_declare(queue='pdfprocess') # Declare a queue
+# send a message
+
+channel.basic_publish(exchange='', routing_key='pdfprocess', body='User information')
+print ("[x] Message sent to consumer")
+connection.close()
