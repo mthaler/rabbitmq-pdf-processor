cce46d55334a69fb87cb15b36060f16b8e3ad0b1
Michael Thaler <michael.thaler@dlr.de>
2021-05-17 10:18:56 +0200

    Updated publisher.py.

--- publisher.py
+++ publisher.py
@@ -1,18 +1,10 @@
-import logging
-import os
+import argparse
 
-logging.basicConfig()
+parser = argparse.ArgumentParser()
+parser.add_argument("user", help="RabbitMQ user")
+parser.add_argument("password", help="RabbitMQ password")
+args = parser.parse_args()
 
-# Parse CLODUAMQP_URL (fallback to localhost)
-url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost/%2f')
-params = pika.URLParameters(url)
-params.socket_timeout = 5
+url = f'amqp://{args.user}:{args.password}@localhost/pdf-processor'
 
-connection = pika.BlockingConnection(params) # Connect to CloudAMQP
-channel = connection.channel() # start a channel
-channel.queue_declare(queue='pdfprocess') # Declare a queue
-# send a message
-
-channel.basic_publish(exchange='', routing_key='pdfprocess', body='User information')
-print ("[x] Message sent to consumer")
-connection.close()
+print(url)
